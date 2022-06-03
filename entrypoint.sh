ca174e72e5e9280716c1b895d28cc7bda13fa2ac
Gray Moss <ddidwyll@gmail.com>
2022-06-04 01:11:11 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="6c301bb8-6a77-41a0-b606-4ba11bbab084"
+  Vless_UUID="6c301ba8-6a77-41a0-b616-4ba11bbab084"
 fi
 echo ${Vless_UUID}
 
@@ -21,7 +21,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="5c302bb8-6c88-41a0-a606-4ba11bbab084"
 fi
 echo ${Vmess_UUID}
 
